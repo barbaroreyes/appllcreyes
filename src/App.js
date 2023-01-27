1d8e45a1c199d2ef0a5c8e252b35fa6a6137caf4
barbaro <48423441+barbaroreyes@users.noreply.github.com>
2023-01-27 15:02:53 -0600

    update

--- src/App.js
+++ src/App.js
@@ -4,21 +4,24 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+
     
        <video  src={video}  className='video-bag'  autoPlay muted loop controls />
      <div className="bg-over"></div>
     <div className="navbar">
       <div className="nav-menu">
+
         <div></div>
         <div></div>
         <div></div>
-        
+       
       </div>
+      <div className="home-text">
+        <h1>Popping Your Rides</h1>
+      
     </div>
-    <div className="home-text">
-      <h1>Reyes Luxury Cars</h1>
-      <p>Popping Your Rides</p>
     </div>
+    <h2>Call Now 737-781-9878</h2>
     </div>   
   );
 }
