08f041d8601a7fabeaaf5e3e7cb718dbca09f513
barbaro <48423441+barbaroreyes@users.noreply.github.com>
2023-01-27 16:52:13 -0600

    update

--- src/App.js
+++ src/App.js
@@ -4,25 +4,23 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-
-    
-       <video  src={video}  className='video-bag'  autoPlay muted loop controls />
+    <video  src={video}  className='video-bag'  autoPlay muted loop controls />
      <div className="bg-over"></div>
     <div className="navbar">
       <div className="nav-menu">
-
         <div></div>
         <div></div>
         <div></div>
-       
+        
       </div>
+      <h2>Popping Your Rides</h2>
       <div className="home-text">
-        <h1>Popping Your Rides</h1>
+        <h1> </h1>
       
     </div>
     </div>
     <footer>
-    <h2>Call Now 737-781-9878</h2>
+    
     <div className="home-btn"><p>Book Now</p></div>
     </footer>
     </div>   
